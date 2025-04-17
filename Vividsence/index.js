5fd95fb137b47d67377e302cfb54aa3617c6def2
AnayKr15hn4 <iamanaykrishna@gmail.com>
2025-04-17 12:19:17 -0400

    Delete Vividsence directory

--- Vividsence/index.js
+++
@@ -1,7 +0,0 @@
-function toggleMenu() {
-    const sidebar = document.getElementById("sidebar");
-    const container = document.querySelector(".container");
-
-    sidebar.classList.toggle("open");
-    container.classList.toggle("shift");
-}
